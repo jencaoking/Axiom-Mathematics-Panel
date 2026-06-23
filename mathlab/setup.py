adf387548787077d77d75083a59a88e39b83d6bf
jencaoking <663930657@qq.com>
2026-06-23 19:49:49 +0800

    fix: set C# LangVersion to 10.0 and bump to v3.6.1

--- mathlab/setup.py
+++ mathlab/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mathlab',
-    version='3.6.0',
+    version='3.6.1',
     description='Interactive Mathematics and AI Teaching Software',
     author='MathLab Team',
     packages=find_packages(),
